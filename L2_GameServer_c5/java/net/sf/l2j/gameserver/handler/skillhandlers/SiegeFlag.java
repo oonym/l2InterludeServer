a47274b1d1dc0ec112d19584331f0ea4c8578e73
Luno <Luno@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2006-11-04 17:23:39 +0000

    ...

--- L2_GameServer_c5/java/net/sf/l2j/gameserver/handler/skillhandlers/SiegeFlag.java
+++ L2_GameServer_c5/java/net/sf/l2j/gameserver/handler/skillhandlers/SiegeFlag.java
@@ -58,15 +58,12 @@ public class SiegeFlag implements ISkillHandler
         
         try
         {
-        	player.sendMessage("1");
             L2ItemInstance itemToTake = player.getInventory().getItemByItemId(SiegeManager.getInstance().getFlagBuyItemId());
-            player.sendMessage("2");
             if(!player.destroyItem("Consume", itemToTake.getObjectId(), SiegeManager.getInstance().getFlagBuyCost(), null, true))
             	return;
-            player.sendMessage("3");
+
             // Spawn a new flag
             L2SiegeFlagInstance flag = new L2SiegeFlagInstance(player, IdFactory.getInstance().getNextId(), NpcTable.getInstance().getTemplate(35062));
-            player.sendMessage("4");
             flag.setTitle(player.getClan().getName());
             flag.setCurrentHpMp(flag.getMaxHp(), flag.getMaxMp());
             flag.setHeading(player.getHeading());
