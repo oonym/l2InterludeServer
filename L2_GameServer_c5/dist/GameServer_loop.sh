6d3f5f4a60994b430e3c0b5007de6ee4200fef62
Lbaldi <Lbaldi@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2006-11-21 21:02:07 +0000

    updated also bsh. Sorry for deleting it in my last commit.

--- L2_GameServer_c5/dist/GameServer_loop.sh
+++ L2_GameServer_c5/dist/GameServer_loop.sh
@@ -7,7 +7,7 @@
 while :; do
 	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
 	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
-	java -Xms512m -Xmx512m -cp bsf.jar:bsh-2.0.jar:commons-logging-1.1.jar:javolution.jar:jython.jar:c3p0-0.9.1-pre10.jar:mysql-connector-java-5.0.4-bin.jar:l2jserver.jar net.sf.l2j.gameserver.GameServer > log/stdout.log 2>&1
+	java -Xms512m -Xmx512m -cp bsf.jar:bsh-2.0b4.jar:commons-logging-1.1.jar:javolution.jar:jython.jar:c3p0-0.9.1-pre10.jar:mysql-connector-java-5.0.4-bin.jar:l2jserver.jar net.sf.l2j.gameserver.GameServer > log/stdout.log 2>&1
 	[ $? -ne 2 ] && break
 #	/etc/init.d/mysql restart
 	sleep 10
