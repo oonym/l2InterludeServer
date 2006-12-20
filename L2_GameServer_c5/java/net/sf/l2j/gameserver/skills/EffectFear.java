c3fcf90d0f3c8f454f2d37831c21d12d7cf2711c
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2006-12-20 09:46:57 +0000

    Fear skills cannot be used on Headquarters Flag. Thanks l2jfree/whiter

--- L2_GameServer_c5/java/net/sf/l2j/gameserver/skills/EffectFear.java
+++ L2_GameServer_c5/java/net/sf/l2j/gameserver/skills/EffectFear.java
@@ -22,6 +22,7 @@ package net.sf.l2j.gameserver.skills;
 import net.sf.l2j.gameserver.ai.CtrlIntention;
 import net.sf.l2j.gameserver.model.L2CharPosition;
 import net.sf.l2j.gameserver.model.L2Effect;
+import net.sf.l2j.gameserver.model.actor.instance.L2NpcInstance;
 import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
 import net.sf.l2j.gameserver.model.actor.instance.L2FolkInstance;
 import net.sf.l2j.gameserver.model.L2Summon;
@@ -68,6 +69,9 @@ final class EffectFear extends L2Effect {
     	// Fear skills cannot be used l2pcinstance to l2pcinstance. Heroic Dread is the exception.
     	if(getEffected() instanceof L2PcInstance && getEffector() instanceof L2PcInstance && getSkill().getId() != 1376) return false;
     	if(getEffected() instanceof L2FolkInstance) return false;
+    	// Fear skills cannot be used on Headquarters Flag.  
+    	if(getEffected() instanceof L2NpcInstance && ((L2NpcInstance)getEffected()).getNpcId() == 35062) return false;  
+
     	if(getEffected() instanceof L2Summon) 
     	{
     		// doesn't affect siege golem
