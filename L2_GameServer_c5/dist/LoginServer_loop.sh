1b1ee140153e6c52ae0d68557c1a80055ccf70e2
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2006-12-21 08:42:59 +0000

    Reverting to old stabile c3p0 version based on tests made by me and ThePhoenixBird (= errors, instability...). If you want to use the newer version, there's already a pre-11 available. Report experiences from that.

--- L2_GameServer_c5/dist/LoginServer_loop.sh
+++ L2_GameServer_c5/dist/LoginServer_loop.sh
@@ -5,7 +5,7 @@ until [ $err == 0 ];
 do
 	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
 	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
-	nice -n -2 java -Xms512m -Xmx512m -cp javolution.jar:c3p0-0.9.1-pre10.jar:mysql-connector-java-5.0.4-bin.jar:l2jserver.jar net.sf.l2j.loginserver.LoginServer > log/stdout.log 2>&1
+	nice -n -2 java -Xms512m -Xmx512m -cp javolution.jar:c3p0-0.9.0.jar:mysql-connector-java-5.0.4-bin.jar:l2jserver.jar net.sf.l2j.loginserver.LoginServer > log/stdout.log 2>&1
 	err=$?
 #	/etc/init.d/mysql restart
 	sleep 10;
