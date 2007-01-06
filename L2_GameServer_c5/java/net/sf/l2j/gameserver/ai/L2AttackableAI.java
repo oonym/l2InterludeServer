74b4580d1e64624acc276548be9c512e35243f47
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-01-06 14:16:03 +0000

    Reverting 402, changed nothing, of course. Knownlist sync needed to fix these NPEs.

--- L2_GameServer_c5/java/net/sf/l2j/gameserver/ai/L2AttackableAI.java
+++ L2_GameServer_c5/java/net/sf/l2j/gameserver/ai/L2AttackableAI.java
@@ -24,7 +24,6 @@ import static net.sf.l2j.gameserver.ai.CtrlIntention.AI_INTENTION_IDLE;
 
 import java.util.Collection;
 import java.util.concurrent.Future;
-import java.util.Iterator; 
 
 import net.sf.l2j.Config;
 import net.sf.l2j.gameserver.GameTimeController;
@@ -320,14 +319,10 @@ public class L2AttackableAI extends L2CharacterAI implements Runnable
             int npcX, npcY, targetX, targetY;
             double dy, dx;
             double dblAggroRange = aggroRange * aggroRange;
-            
-            L2Object obj = null;
-            
-            for (Iterator<L2Object> i = npc.getKnownList().getKnownObjects().iterator(); i.hasNext(); )  
-            {  
-            	obj = i.next();  
-
-            	if (obj == null || !(obj instanceof L2Character)) continue;
+            // Go through visible objects
+            for (L2Object obj : npc.getKnownList().getKnownObjects())
+            {
+                if (obj == null || !(obj instanceof L2Character)) continue;
 
                 npcX = npc.getX();
                 npcY = npc.getY();
