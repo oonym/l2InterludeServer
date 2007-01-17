250f8797120a758f29c13ff3d43775bcb15f90a7
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-01-17 07:28:50 +0000

    Fix for #424, thanks Fulminus

--- L2_GameServer_c5/java/net/sf/l2j/gameserver/model/L2Character.java
+++ L2_GameServer_c5/java/net/sf/l2j/gameserver/model/L2Character.java
@@ -587,7 +587,7 @@ public abstract class L2Character extends L2Object
         		if (player.isCursedWeaponEquiped())
         		{
                 	// If hitted by a cursed weapon, Cp is reduced to 0
-        			if (!(target instanceof L2PcInstance) || !((L2PcInstance)target).isInvul())
+        			if (target instanceof L2PcInstance && !((L2PcInstance)target).isInvul())
         				target.setCurrentCp(0);
         		} else if (player.isHero())
         		{
