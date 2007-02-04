d1b1ff909fe2b644177a8af1828b43fe2dc70edb
Lbaldi <Lbaldi@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-02-04 13:33:11 +0000

    Now also normal players can buy from Fishermen ;)

--- L2_GameServer_c5/java/net/sf/l2j/gameserver/clientpackets/RequestBuyItem.java
+++ L2_GameServer_c5/java/net/sf/l2j/gameserver/clientpackets/RequestBuyItem.java
@@ -104,7 +104,7 @@ public class RequestBuyItem extends ClientBasePacket
 
         L2Object target = player.getTarget();
         if (!player.isGM() && (target == null								// No target (ie GM Shop)
-        		|| !(target instanceof L2MerchantInstance || target instanceof L2MercManagerInstance)	// Target not a merchant and not mercmanager
+        		|| !(target instanceof L2MerchantInstance || target instanceof L2FishermanInstance || target instanceof L2MercManagerInstance)	// Target not a merchant, fisherman or mercmanager
 			    || !player.isInsideRadius(target, L2NpcInstance.INTERACTION_DISTANCE, false, false) 	// Distance is too far
 			        )) return;
 
