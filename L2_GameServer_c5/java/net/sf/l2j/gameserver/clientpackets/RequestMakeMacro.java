08fb70748d42f31f169b8410e98b352985d2bc5e
Nemesiss <Nemesiss@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-02-14 19:21:42 +0000

    Fix for #432

--- L2_GameServer_c5/java/net/sf/l2j/gameserver/clientpackets/RequestMakeMacro.java
+++ L2_GameServer_c5/java/net/sf/l2j/gameserver/clientpackets/RequestMakeMacro.java
@@ -25,11 +25,13 @@ import net.sf.l2j.gameserver.ClientThread;
 import net.sf.l2j.gameserver.model.L2Macro;
 import net.sf.l2j.gameserver.model.L2Macro.L2MacroCmd;
 import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
+import net.sf.l2j.gameserver.serverpackets.SystemMessage;
 
 public class RequestMakeMacro extends ClientBasePacket
 {
 
 	private final L2Macro _macro;
+	private int _commands_lenght = 0;
     
 	private static final String _C__C1_REQUESTMAKEMACRO = "[C] C1 RequestMakeMacro";
 	
@@ -73,6 +75,7 @@ public class RequestMakeMacro extends ClientBasePacket
             int d1         = readD(); // skill or page number for shortcuts
             int d2         = readC();
             String command = readS();
+            _commands_lenght += command.length();
 			commands[i] = new L2MacroCmd(entry, type, d1, d2, command);
             if (Config.DEBUG) System.out.println("entry:"+entry+"\ttype:"+type+"\td1:"+d1+"\td2:"+d2+"\tcommand:"+command);
         }
@@ -84,6 +87,30 @@ public class RequestMakeMacro extends ClientBasePacket
 		L2PcInstance  player = getClient().getActiveChar(); 
 		if (player == null)
 		    return;
+		if (_commands_lenght > 255)
+		{
+			//Invalid macro. Refer to the Help file for instructions.
+			player.sendPacket(new SystemMessage(810));
+			return;
+		}
+		if (player.getMacroses().getAllMacroses().length > 5)
+		{
+			//You may create up to 24 macros.
+			player.sendPacket(new SystemMessage(797));
+			return;
+		}
+		if (_macro.name.length() == 0)
+		{
+			//Enter the name of the macro.
+			player.sendPacket(new SystemMessage(838));
+			return;
+		}
+		if (_macro.descr.length() > 32)
+		{
+			//Macro descriptions may contain up to 32 characters.
+			player.sendPacket(new SystemMessage(837));
+			return;
+		}
 		player.registerMacro(_macro);
 	}
 
