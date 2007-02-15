6b668e940511be4114b7aa74e8e6094b28dccba0
Luno <Luno@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-02-15 11:41:55 +0000

    !L2Skill: clean up

--- L2_GameServer_c5/java/net/sf/l2j/gameserver/model/L2Skill.java
+++ L2_GameServer_c5/java/net/sf/l2j/gameserver/model/L2Skill.java
@@ -87,19 +87,136 @@ public abstract class L2Skill
     }
 
     /** Target types of skills : SELF, PARTY, CLAN, PET... */
-    public static enum SkillTargetType {
-        TARGET_NONE, TARGET_SELF, TARGET_ONE, TARGET_PARTY, TARGET_ALLY, TARGET_CLAN, TARGET_PET, TARGET_AREA, TARGET_AURA, TARGET_CORPSE, TARGET_UNDEAD, TARGET_AREA_UNDEAD, TARGET_MULTIFACE, TARGET_CORPSE_ALLY, TARGET_CORPSE_CLAN, TARGET_CORPSE_PLAYER, TARGET_CORPSE_PET, TARGET_ITEM, TARGET_AREA_CORPSE_MOB, TARGET_CORPSE_MOB, TARGET_UNLOCKABLE, TARGET_HOLY, TARGET_PARTY_MEMBER, TARGET_ENEMY_SUMMON, TARGET_OWNER_PET
+    public static enum SkillTargetType 
+    {
+        TARGET_NONE,
+        TARGET_SELF,
+        TARGET_ONE,
+        TARGET_PARTY,
+        TARGET_ALLY,
+        TARGET_CLAN,
+        TARGET_PET,
+        TARGET_AREA,
+        TARGET_AURA,
+        TARGET_CORPSE,
+        TARGET_UNDEAD,
+        TARGET_AREA_UNDEAD,
+        TARGET_MULTIFACE,
+        TARGET_CORPSE_ALLY,
+        TARGET_CORPSE_CLAN,
+        TARGET_CORPSE_PLAYER,
+        TARGET_CORPSE_PET,
+        TARGET_ITEM, 
+        TARGET_AREA_CORPSE_MOB, 
+        TARGET_CORPSE_MOB, 
+        TARGET_UNLOCKABLE,
+        TARGET_HOLY, 
+        TARGET_PARTY_MEMBER, 
+        TARGET_ENEMY_SUMMON,
+        TARGET_OWNER_PET
     }
 
-    public static enum SkillType {
-    	PDAM, MDAM, DOT, BLEED, POISON, HEAL, HOT, COMBATPOINTHEAL, CPHOT, MANAHEAL, MANAHEAL_PERCENT, MANARECHARGE, MPHOT, AGGDAMAGE, BUFF, DEBUFF, STUN, ROOT, RESURRECT, PASSIVE, CONT, CONFUSION, UNLOCK, CHARGE(
-                L2SkillCharge.class), FEAR, MHOT, DRAIN(L2SkillDrain.class), NEGATE, CANCEL,
-                SLEEP, AGGREDUCE, AGGREMOVE, AGGREDUCE_CHAR, CHARGEDAM(
-                L2SkillChargeDmg.class), CONFUSE_MOB_ONLY, DEATHLINK, DETECT_WEAKNESS, ENCHANT_ARMOR, ENCHANT_WEAPON, FEED_PET, HEAL_PERCENT, LUCK, MANADAM, MDOT, MUTE, RECALL, REFLECT, SOULSHOT, SPIRITSHOT, SPOIL, SWEEP, SUMMON(
-                L2SkillSummon.class), WEAKNESS, DEATHLINK_PET, MANA_BY_LEVEL, FAKE_DEATH, UNBLEED, UNPOISON, SIEGEFLAG, TAKECASTLE, UNDEAD_DEFENSE, SEED(
-                L2SkillSeed.class), PARALYZE, DRAIN_SOUL, COMMON_CRAFT, DWARVEN_CRAFT, WEAPON_SA, FISHING, PUMPING, REELING, CREATE_ITEM(
-                L2SkillCreateItem.class), AGGDEBUFF, STRSIEGEASSAULT, ERASE, MAGE_BANE, WARRIOR_BANE,SUMMON_TREASURE_KEY,BETRAY,BALANCE_LIFE,
-
+    public static enum SkillType 
+    {
+    	// Damage
+    	PDAM,
+    	MDAM,
+    	MANADAM,
+    	DOT,
+    	MDOT, 
+    	DRAIN_SOUL,
+    	DRAIN(L2SkillDrain.class),
+    	DEATHLINK,
+    	
+    	// Disablers
+    	BLEED,
+    	POISON,
+    	STUN, 
+    	ROOT, 
+    	CONFUSION,
+    	FEAR,
+    	SLEEP,
+    	CONFUSE_MOB_ONLY,
+    	MUTE,
+    	PARALYZE,
+    	WEAKNESS,
+    	
+    	// hp, mp, cp
+    	HEAL,
+    	HOT, 
+    	BALANCE_LIFE,
+    	HEAL_PERCENT, 
+    	COMBATPOINTHEAL,
+    	CPHOT,
+    	MANAHEAL,
+    	MANA_BY_LEVEL,
+    	MANAHEAL_PERCENT,
+    	MANARECHARGE, 
+    	MPHOT, 
+    	
+    	// Aggro
+    	AGGDAMAGE, 
+    	AGGREDUCE,
+    	AGGREMOVE,
+    	AGGREDUCE_CHAR,
+    	AGGDEBUFF, 
+    	
+   	    // Fishing
+    	FISHING,
+    	PUMPING,
+    	REELING,
+    	
+    	// MISC
+    	UNLOCK,
+    	ENCHANT_ARMOR, 
+    	ENCHANT_WEAPON,
+    	SOULSHOT, 
+    	SPIRITSHOT,
+    	SIEGEFLAG, 
+    	TAKECASTLE,
+    	WEAPON_SA,
+    	
+    	// Creation
+    	COMMON_CRAFT,
+    	DWARVEN_CRAFT,
+    	CREATE_ITEM(L2SkillCreateItem.class), 
+    	SUMMON_TREASURE_KEY,
+    	
+    	// Summons
+    	SUMMON(L2SkillSummon.class),
+    	FEED_PET, 
+    	DEATHLINK_PET,
+    	STRSIEGEASSAULT, 
+    	ERASE,
+    	BETRAY,
+    	
+    	// Cancel
+    	CANCEL,
+    	MAGE_BANE,
+    	WARRIOR_BANE,
+    	NEGATE,
+    	
+    	BUFF, 
+    	DEBUFF,
+    	PASSIVE,
+    	CONT, 
+    	
+    	RESURRECT,
+    	CHARGE(L2SkillCharge.class), 
+    	MHOT,
+    	CHARGEDAM(L2SkillChargeDmg.class), 
+    	DETECT_WEAKNESS, 
+    	LUCK,
+    	RECALL,
+    	REFLECT,
+    	SPOIL,
+    	SWEEP,
+    	FAKE_DEATH,
+    	UNBLEED,
+    	UNPOISON, 
+    	UNDEAD_DEFENSE, 
+    	SEED (L2SkillSeed.class), 
+    	 
         // unimplemented
         NOTDONE;
 
