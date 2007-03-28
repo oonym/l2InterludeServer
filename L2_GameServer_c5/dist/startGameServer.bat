d0bdbc9341b4e3df2f4ab9b1f8503b119c89dbea
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-03-28 07:50:14 +0000

    Adding some config info to startGameServer.bat

--- L2_GameServer_c5/dist/startGameServer.bat
+++ L2_GameServer_c5/dist/startGameServer.bat
@@ -2,7 +2,13 @@
 :start
 echo Starting L2J Game Server.
 echo.
+REM -------------------------------------
+REM Default parameters for a basic server.
 java -Xmx512m -cp bsf.jar;bsh-2.0b4.jar;commons-logging-1.1.jar;javolution.jar;c3p0-0.9.1.jar;mysql-connector-java-5.0.4-bin.jar;l2jserver.jar;jython.jar net.sf.l2j.gameserver.GameServer
+REM
+REM If you have a big server and lots of memory, you could experiment for example with
+REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts
+REM -------------------------------------
 if ERRORLEVEL 2 goto restart
 if ERRORLEVEL 1 goto error
 goto end
