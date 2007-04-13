4f3a015760701202dffd96575e964d26fd764435
Fulminus <Fulminus@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-04-13 21:01:31 +0000

    Fix for http://forum.l2jdp.com/viewtopic.php?t=6345  (UNTESTED) Thanks Scafu for notifying me ^_^

--- L2_GameServer_c5/java/net/sf/l2j/gameserver/clientpackets/MultiSellChoose.java
+++ L2_GameServer_c5/java/net/sf/l2j/gameserver/clientpackets/MultiSellChoose.java
@@ -147,12 +147,19 @@ public class MultiSellChoose extends ClientBasePacket
 				// a) if enchantment is maintained, then get a list of items that exactly match this enchantment
 				if (maintainEnchantment)
 				{
+					L2ItemInstance[] inventoryContents = null;
 					// loop through this list and remove (one by one) each item until the required amount is taken.
-					L2ItemInstance[] inventoryContents = inv.getAllItemsByItemId(e.getItemId(), _enchantment);
+	            	L2Item tempItem = ItemTable.getInstance().createDummyItem(e.getItemId()).getItem();
+	            	if ((tempItem instanceof L2Armor) || (tempItem instanceof L2Weapon))
+	            		inventoryContents = inv.getAllItemsByItemId(e.getItemId(), _enchantment);
+	            	else
+	            		inventoryContents = inv.getAllItemsByItemId(e.getItemId(), 0);
+					
         			synchronized (inventoryContents)
         			{
-		                for (int i = 0; i < (e.getItemCount() * _amount); i++)
-							player.destroyItem("Multisell", inventoryContents[i].getObjectId(), 1, player.getTarget(), true);
+        				if (inventoryContents != null && inventoryContents.length > 0)
+			                for (int i = 0; i < (e.getItemCount() * _amount); i++)
+								player.destroyItem("Multisell", inventoryContents[i].getObjectId(), 1, player.getTarget(), true);
         			}
 				}
 				else	// b) enchantment is not maintained.  Get the instances with the LOWEST enchantment level
