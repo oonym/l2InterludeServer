c5aea71cb9050377927b6440c098209866b95527
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-05-13 22:40:31 +0000

    Lib update: c3p0-0.9.1.1. Thanks Melerix

--- L2_GameServer_It/dist/startGameServer.bat
+++ L2_GameServer_It/dist/startGameServer.bat
@@ -4,7 +4,7 @@ echo Starting L2J Game Server.
 echo.
 REM -------------------------------------
 REM Default parameters for a basic server.
-java -Xmx512m -cp bsf.jar;bsh-2.0b4.jar;commons-logging-1.1.jar;mmocore.jar;javolution.jar;c3p0-0.9.1.jar;mysql-connector-java-5.0.5-bin.jar;l2jserver.jar;jython.jar net.sf.l2j.gameserver.GameServer
+java -Xmx512m -cp bsf.jar;bsh-2.0b4.jar;commons-logging-1.1.jar;mmocore.jar;javolution.jar;c3p0-0.9.1.1.jar;mysql-connector-java-5.0.5-bin.jar;l2jserver.jar;jython.jar net.sf.l2j.gameserver.GameServer
 REM
 REM If you have a big server and lots of memory, you could experiment for example with
 REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts
--- L2_GameServer_It/dist/startLoginServer.bat
+++ L2_GameServer_It/dist/startLoginServer.bat
@@ -2,7 +2,7 @@
 :start
 echo Starting L2J Login Server.
 echo.
-java -Xmx128m -cp javolution.jar;mmocore.jar;c3p0-0.9.1.jar;mysql-connector-java-5.0.5-bin.jar;l2jserver.jar net.sf.l2j.loginserver.L2LoginServer
+java -Xmx128m -cp javolution.jar;mmocore.jar;c3p0-0.9.1.1.jar;mysql-connector-java-5.0.5-bin.jar;l2jserver.jar net.sf.l2j.loginserver.L2LoginServer
 if ERRORLEVEL 2 goto restart
 if ERRORLEVEL 1 goto error
 goto end
