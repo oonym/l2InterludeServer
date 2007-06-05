348b1bee66f2ffddd01fb1c44833a76f37eccce8
Fulminus <Fulminus@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-06-05 14:39:38 +0000

    Just some comments added to a pet packet.  It's not really used, though

--- L2_GameServer_It/java/net/sf/l2j/gameserver/serverpackets/PetDelete.java
+++ L2_GameServer_It/java/net/sf/l2j/gameserver/serverpackets/PetDelete.java
@@ -28,15 +28,15 @@ public class PetDelete extends L2GameServerPacket
 	
 	public PetDelete(int petId, int petnum)
 	{
-		_petId = petId;
-		_petnum= petnum;
+		_petId = petId;		// summonType?
+		_petnum= petnum;	//objectId
 	}
 	
 	protected final void writeImpl()
 	{
 		writeC(0xb6);
 		writeD(_petId);// dont really know what these two are since i never needed them
-		writeD(_petnum);
+		writeD(_petnum);//objectId
 	}
 	
 	public String getType()
