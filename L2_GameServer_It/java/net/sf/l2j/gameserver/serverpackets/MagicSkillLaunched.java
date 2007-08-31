5d76f9c50544d7f09e0a3ddbeb1fee6b85a2b689
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-08-31 06:47:16 +0000

    for #1389

--- L2_GameServer_It/java/net/sf/l2j/gameserver/serverpackets/MagicSkillLaunched.java
+++ L2_GameServer_It/java/net/sf/l2j/gameserver/serverpackets/MagicSkillLaunched.java
@@ -70,7 +70,7 @@ public class MagicSkillLaunched extends L2GameServerPacket
 		writeD(_skillId);
 		writeD(_skillLevel);
 		writeD(_numberOfTargets); // also failed or not?
-		if (_singleTargetId > 0 || _numberOfTargets == 0) 
+		if (_singleTargetId != 0 || _numberOfTargets == 0) 
 			writeD(_singleTargetId);
 		else for(L2Object target : _targets)
 		{
