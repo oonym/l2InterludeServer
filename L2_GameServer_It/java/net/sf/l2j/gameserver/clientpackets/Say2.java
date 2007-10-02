4fb182d6b3ffdb1432984671d44df35b124be4a4
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-10-02 19:57:42 +0000

    BufferUnderflowException in Say2

--- L2_GameServer_It/java/net/sf/l2j/gameserver/clientpackets/Say2.java
+++ L2_GameServer_It/java/net/sf/l2j/gameserver/clientpackets/Say2.java
@@ -18,6 +18,7 @@
  */
 package net.sf.l2j.gameserver.clientpackets;
 
+import java.nio.BufferUnderflowException;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
@@ -89,7 +90,14 @@ public final class Say2 extends L2GameClientPacket
 	protected void readImpl()
 	{
 		_text = readS();
-		_type = readD();
+		try
+		{
+			_type = readD();
+		}
+		catch (BufferUnderflowException e) 
+		{ 
+			_type = CHAT_NAMES.length;
+		}
 		_target = (_type == TELL) ? readS() : null;
 	}
 	
