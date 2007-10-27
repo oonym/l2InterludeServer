c3a12916c7fdaa02bb8f17efaf384636d45c50d0
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-10-27 20:16:28 +0000

    Commit for Kerberos_20: if player is feared, he cannot use skills

--- L2_GameServer_It/java/net/sf/l2j/gameserver/clientpackets/RequestMagicSkillUse.java
+++ L2_GameServer_It/java/net/sf/l2j/gameserver/clientpackets/RequestMagicSkillUse.java
@@ -69,7 +69,11 @@ public final class RequestMagicSkillUse extends L2GameClientPacket
 			activeChar.sendPacket(new ActionFailed());
 			return; 
 		}
-        
+        if (activeChar.isAffraid())
+        {
+        	activeChar.sendPacket(new ActionFailed());
+        	return;
+        }
         // Get the L2Skill template corresponding to the skillID received from the client
 		L2Skill skill = SkillTable.getInstance().getInfo(_magicId, level);
         
