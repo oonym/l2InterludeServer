5970bf4b92831d3dc7ace43ec1df0a1b673d8b29
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-10-28 10:53:14 +0000

    reverting [1274] because of new info

--- L2_GameServer_It/java/net/sf/l2j/gameserver/clientpackets/RequestSocialAction.java
+++ L2_GameServer_It/java/net/sf/l2j/gameserver/clientpackets/RequestSocialAction.java
@@ -21,7 +21,6 @@ package net.sf.l2j.gameserver.clientpackets;
 import java.util.logging.Logger;
 
 import net.sf.l2j.Config;
-import net.sf.l2j.gameserver.ThreadPoolManager;
 import net.sf.l2j.gameserver.ai.CtrlIntention;
 import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
 import net.sf.l2j.gameserver.network.SystemMessageId;
@@ -82,11 +81,14 @@ public class RequestSocialAction extends L2GameClientPacket
 			
 			SocialAction atk = new SocialAction(activeChar.getObjectId(), _actionId);
 			activeChar.broadcastPacket(atk);
+			/*
 			// Schedule a social task to wait for the animation to finish
 			ThreadPoolManager.getInstance().scheduleGeneral(new SocialTask(this), 2600);
 			activeChar.setIsParalyzed(true);
+			*/
 		}
 	}
+	/*
 	class SocialTask implements Runnable
 	{
 		L2PcInstance _player;
@@ -99,6 +101,8 @@ public class RequestSocialAction extends L2GameClientPacket
 			_player.setIsParalyzed(false);
 		}
 	}
+	*/
+	
 	/* (non-Javadoc)
 	 * @see net.sf.l2j.gameserver.clientpackets.ClientBasePacket#getType()
 	 */
