418d1ecd610501137304ce05a1da9dab1bfb3861
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2007-12-08 20:53:21 +0000

    Fix for #1742, thanks xTr, slyce

--- L2_GameServer_It/java/net/sf/l2j/gameserver/serverpackets/NpcInfo.java
+++ L2_GameServer_It/java/net/sf/l2j/gameserver/serverpackets/NpcInfo.java
@@ -55,8 +55,8 @@ public class NpcInfo extends L2GameServerPacket
 		_activeChar = cha;
 		_idTemplate = cha.getTemplate().idTemplate;
 		_isAttackable = cha.isAutoAttackable(attacker);
-		_rhand = cha.getLeftHandItem();
-		_lhand = cha.getRightHandItem();
+		_rhand = cha.getRightHandItem(); 
+		_lhand = cha.getLeftHandItem(); 
 		_isSummoned = false;
         _collisionHeight = _activeChar.getTemplate().collisionHeight;
         _collisionRadius = _activeChar.getTemplate().collisionRadius;
